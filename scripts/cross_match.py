30b2f518bb18badfa7fa5ac1e204101ae77c828c
SinclaM <matthewsinclair2024@u.northwestern.edu>
2023-06-15 14:46:28 +0100

    Use 5 arcsecond separation in cross matching, copy column names from result

--- scripts/cross_match.py
+++ scripts/cross_match.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 from __future__ import annotations
-import csv
 import os
 from io import StringIO
 
@@ -9,6 +8,7 @@ from sqlalchemy import URL, create_engine, text
 from sqlalchemy.orm import sessionmaker
 
 from sneparse.definitions import ROOT_DIR
+from sneparse.coordinates import DecimalDegrees, DegreesMinutesSeconds
 from sneparse.db.models import CLEANED_TABLE_NAME
 from sneparse.util import unwrap
 
@@ -27,7 +27,7 @@ if __name__ == "__main__":
     local_session_maker = sessionmaker(local_engine)  
     with local_session_maker() as session:
         # Copy local catalog to memory
-        copy_to = f"COPY {CLEANED_TABLE_NAME} TO STDOUT WITH (FORMAT csv)"
+        copy_to = f"COPY {CLEANED_TABLE_NAME} TO STDOUT WITH (FORMAT csv);\n"
         print(copy_to)
         session.connection().connection.cursor().copy_expert(copy_to, catalog_buffer)
 
@@ -51,29 +51,29 @@ if __name__ == "__main__":
         # Ideally, we could use posgres_fdw for this, but it is far too slow.
         temp_catalog = "temp_catalog"
         create_temp_catalog = text(
-            f"""CREATE TEMPORARY TABLE {temp_catalog} (
-                cleaned_id integer NOT NULL,
-                master_id integer NOT NULL,
-                name character varying NOT NULL,
-                right_ascension double precision,
-                declination double precision,
-                discover_date timestamp without time zone,
-                claimed_type character varying,
-                source text NOT NULL
-            );"""
+            f"CREATE TEMPORARY TABLE {temp_catalog} (       \n"
+             "    cleaned_id integer NOT NULL,              \n"
+             "    master_id integer NOT NULL,               \n"
+             "    name character varying NOT NULL,          \n"
+             "    right_ascension double precision,         \n"
+             "    declination double precision,             \n"
+             "    discover_date timestamp without time zone,\n"
+             "    claimed_type character varying,           \n"
+             "    source text NOT NULL                      \n"
+             ");                                            \n"
         )
         print(create_temp_catalog)
         session.execute(create_temp_catalog)
 
-        copy_from = f"COPY {temp_catalog} FROM STDIN WITH (FORMAT csv)"
+        copy_from = f"COPY {temp_catalog} FROM STDIN WITH (FORMAT csv);\n"
         print(copy_from)
         session.connection().connection.cursor().copy_expert(copy_from, catalog_buffer)
 
         # Prepare the temp catalog table for cross matching
         q3c_prepare_catalog = text(
-            f"""CREATE INDEX ON {temp_catalog} (q3c_ang2ipix("right_ascension", "declination"));
-                CLUSTER {temp_catalog}_q3c_ang2ipix_idx ON {temp_catalog};
-                ANALYZE {temp_catalog};"""
+            f"CREATE INDEX ON {temp_catalog} (q3c_ang2ipix(\"right_ascension\", \"declination\"));\n"
+            f"CLUSTER {temp_catalog}_q3c_ang2ipix_idx ON {temp_catalog};                          \n"
+            f"ANALYZE {temp_catalog};                                                             \n"
         )
         print(q3c_prepare_catalog)
         session.execute(q3c_prepare_catalog);
@@ -81,30 +81,40 @@ if __name__ == "__main__":
         # The foreign VLASS sources table is not indexed properly, so we create a temp
         # copy of it and prepare it for cross matching.
         temp_gaussian = "temp_gaussian"
-        create_temp_gaussian = text(f"CREATE TEMPORARY TABLE {temp_gaussian} AS TABLE pybdsf_gaussian_epoch1")
+        create_temp_gaussian = text(f"CREATE TEMPORARY TABLE {temp_gaussian} AS TABLE pybdsf_gaussian_epoch1;\n")
         print(create_temp_gaussian)
         session.execute(create_temp_gaussian)
 
         q3c_prepare_gaussian = text(
-            f"""CREATE INDEX ON {temp_gaussian} (q3c_ang2ipix("ra", "decl"));
-                CLUSTER {temp_gaussian}_q3c_ang2ipix_idx ON {temp_gaussian};
-                ANALYZE {temp_gaussian};"""
+            f"CREATE INDEX ON {temp_gaussian} (q3c_ang2ipix(\"ra\", \"decl\"));\n"
+            f"CLUSTER {temp_gaussian}_q3c_ang2ipix_idx ON {temp_gaussian};     \n"
+            f"ANALYZE {temp_gaussian};                                         \n"
         )
         print(q3c_prepare_gaussian)
         session.execute(q3c_prepare_gaussian);
 
         # Do the actual cross matching.
+        separation = DecimalDegrees.from_dms(DegreesMinutesSeconds(1, 0, 0, 5)).degrees
+
+        temp_cross_match = "temp_cross_match"
         cross_match = text(
-            f"""SELECT * FROM {temp_catalog} AS a, {temp_gaussian} AS b 
-                where q3c_join(a.right_ascension, a.declination, b.ra, b.decl, 0.000555556)"""
+            f"CREATE TEMPORARY TABLE {temp_cross_match} AS                                    \n"
+            f"    SELECT * FROM {temp_catalog} AS a, {temp_gaussian} AS b                     \n"
+            f"    WHERE q3c_join(a.right_ascension, a.declination, b.ra, b.decl, {separation})\n"
+            f"        AND a.discover_date < TIMESTAMP '2019-08-01'                            \n"
         )
         print(cross_match)
-        matches = session.execute(cross_match).all()
+        session.execute(cross_match)
+
+        # Copy cross matches into memory
+        cross_matches_buffer = StringIO()
+        copy_cross_matches = f"COPY {temp_cross_match} TO STDOUT WITH (FORMAT csv, HEADER);\n"
+        print(copy_cross_matches)
+        session.connection().connection.cursor().copy_expert(copy_cross_matches, cross_matches_buffer)
+        cross_matches_buffer.seek(0)
 
     output_file = ROOT_DIR.joinpath("resources", "cross_matches.csv")
-    print(f"\nWriting results to {output_file}")
+    print(f"Writing results to {output_file}")
     with open(output_file, "w") as csvfile:
         # Save the results to an output file.
-        writer = csv.writer(csvfile, delimiter=',')
-        for match_ in matches:
-            writer.writerow(match_.tuple())
+        print(cross_matches_buffer.getvalue(), file=csvfile)
